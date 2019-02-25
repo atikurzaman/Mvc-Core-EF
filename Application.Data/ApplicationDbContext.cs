ba2decfed41f53a4ec23d3ea990327e6a57c8408
Kaptan <Kaptan.cse@gmail.com>
2019-02-25 22:46:13 +0600

    commit

--- Application.Data/ApplicationDbContext.cs
+++ Application.Data/ApplicationDbContext.cs
@@ -8,12 +8,12 @@ namespace Application.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public DbSet<User> Users { get; set; }
-        public DbSet<Role> Roles { get; set; }
-        public DbSet<UserRole> UserRoles { get; set; }
-        public DbSet<UserLogin> UserLogins { get; set; }
-        public DbSet<UserClaim> UserClaims { get; set; }
-        public DbSet<RoleClaim> RoleClaims { get; set; }
+        //public DbSet<User> Users { get; set; }
+        //public DbSet<Role> Roles { get; set; }
+        //public DbSet<UserRole> UserRoles { get; set; }
+        //public DbSet<UserLogin> UserLogins { get; set; }
+        //public DbSet<UserClaim> UserClaims { get; set; }
+        //public DbSet<RoleClaim> RoleClaims { get; set; }
         public DbSet<Menu> Menus { get; set; }        
         public DbSet<Category> Categories { get; set; }
         public DbSet<Attribute> ProductAttributes { get; set; }
